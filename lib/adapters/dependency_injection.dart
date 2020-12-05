f355eb6bd58ea09126fe8d0192e8f3aff6727547
Eliah7 <eliahmbwilo7@gmail.com>
2020-12-05 23:17:46 +0300

    Init get_it for dependency injectin

---
+++ lib/adapters/dependency_injection.dart
@@ -0,0 +1,22 @@
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+
+abstract class BFastDependencyInjectionController {
+  final getIt = GetIt.instance;
+
+  /**
+   * Implement method and return $initGetIt(getIt)
+   * run the commands 
+   *  - flutter packages pub run build_runner watch
+   * this will generate a file containing all annotations
+   * 
+   */
+  @InjectableInit(
+    initializerName: r'$initGetIt', // default
+    preferRelativeImports: true, // default
+    asExtension: false, // default
+  )
+  void configureDependencies() => $initGetIt(getIt);
+
+  
+}
