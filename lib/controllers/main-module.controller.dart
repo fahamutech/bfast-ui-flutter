6c2e68c3465ab360289553a89c06279e36e618ba
joshua mshana <mama27j@gmail.com>
2021-06-21 09:19:20 +0300

    fix start method of main module

--- lib/controllers/main-module.controller.dart
+++ lib/controllers/main-module.controller.dart
@@ -3,11 +3,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-typedef MaterialApp Bootstrap();
+typedef MaterialApp _Bootstrap();
+
+class _AppWidget extends StatelessWidget {
+  final MaterialApp _component;
+
+  _AppWidget(this._component);
+
+  @override
+  Widget build(BuildContext context) {
+    return this._component.modular();
+  }
+}
 
 class MainModuleController {
   final MainModuleAdapter module;
-  final Bootstrap component;
+  final _Bootstrap component;
 
   MainModuleController({@required this.module, @required this.component});
 
@@ -18,7 +29,7 @@ class MainModuleController {
 
     return ModularApp(
       module: this.module,
-      child: this.component().modular(),
+      child: _AppWidget(this.component()),
     );
   }
 }
