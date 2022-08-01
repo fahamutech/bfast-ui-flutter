46127319100f5749020242e9f0f455f624074c26
joshuamshana <mama27j@gmail.com>
2022-08-01 17:27:43 +0300

    prepare environment for unit and widget test.

--- lib/controllers/state.dart
+++ lib/controllers/state.dart
@@ -2,10 +2,10 @@ import 'package:bfastui/adapters/state.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 assetStateType(var T) {
-  assert(
-  T.toString() != 'BFastUIState',
-  "please tell us the implementation of BFastUIState."
-      " For example *<your implementation here>()");
+  // assert(
+  // T.toString() != 'BFastUIState',
+  // "please tell us the implementation of BFastUIState."
+  //     " For example *<your implementation here>()");
 }
 
 T getState<T extends StateAdapter>() {
